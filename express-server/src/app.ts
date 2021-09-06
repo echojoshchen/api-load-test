70bcf57853e48c601fb1ebecc6565bfeb8f54f19
Joshua Chen <jlchen@bethel.jw.org>
2021-09-06 09:58:05 -0400

    update load test script

--- express-server/src/app.ts
+++ express-server/src/app.ts
@@ -2,23 +2,11 @@
  * Setup routes
  */
 import express from "express";
-import helmet from "helmet";
 
 export function configureRouter(app: express.Router): void {
-    // Set security headers
-    app.use(
-        helmet({
-            frameguard: { action: "deny" },
-            contentSecurityPolicy: {
-                directives: {
-                    defaultSrc: ["'self'"],
-                },
-            },
-        }),
-    );
 
     app.get("/", (_: express.Request, res: express.Response) => {
-        res.send("hello world");
+        res.send(Math.random().toString());
     });
 }
 
